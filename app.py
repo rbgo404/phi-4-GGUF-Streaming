f6308a4b16da8a2a89354486a0f544e2c95965a9
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-03-04 14:17:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class InferlessPythonModel:
         snapshot_download(repo_id=model_id,allow_patterns=["phi-4-Q4_K_M.gguf"])
         self.llm = Llama.from_pretrained(repo_id=model_id,filename="phi-4-Q4_K_M.gguf",main_gpu=0,n_gpu_layers=-1)
       
-    def infer(self, inputs):
+    def infer(self, inputs,stream_output_handler):
         prompt = inputs["prompt"]
         system_prompt = inputs.get("system_prompt","You are a friendly bot.")
         temperature = inputs.get("temperature",0.7)
@@ -22,11 +22,19 @@ class InferlessPythonModel:
                     messages = [
                       {"role": "system", "content": f"{system_prompt}"},
                       {"role": "user","content": f"{prompt}"}],
-                    temperature=temperature, top_p=top_p, top_k=top_k,repeat_penalty=repeat_penalty,max_tokens=max_tokens
+                    temperature=temperature, top_p=top_p, top_k=top_k,repeat_penalty=repeat_penalty,max_tokens=max_tokens,
+                    stream=True
         )
-        text_result = output['choices'][0]['message']['content']
+        for token_data in output:
+            delta = token_data["choices"][0].get("delta", {})
+            token_text = delta.get("content", "")
+            full_response += token_text
+            if token_text:
+                output_dict = {}
+                output_dict["OUT"] = token_text
+                stream_output_handler.send_streamed_output(output_dict)
         
-        return {'generated_result': text_result}
+        stream_output_handler.finalise_streamed_output()
         
     def finalize(self):
         self.llm = None
